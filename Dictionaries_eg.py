e212772c4a95d91d7066123f136aa1c9e6f3731e
Shreyas V Bhatt <shreyas.bhatt@zapcg.com>
2024-10-22 17:28:41 +0530

    Date 22/10

---
+++ Dictionaries_eg.py
@@ -0,0 +1,8 @@
+my_dict = {'name': 'Alice', 'age': 25, 'city': 'Wonderland'}
+print("Dictionary:", my_dict)
+
+
+#Accessing Elements in a Dictionary
+print("Name:", my_dict['name'])
+print("Age:", my_dict['age'])
+print("City:", my_dict['city'])
---
+++ sets_eg.py
@@ -0,0 +1,16 @@
+my_set = set([1, 2, 3, 4, 4]) 
+print("Set:", my_set)
+
+
+#Accessing Elements in a Set
+my_list = list(my_set)
+print("First element:", my_list[0]) 
+
+
+#Adding Elements to a Set  
+my_set.add(5)
+print("Set:", my_set)
+
+#Removing Elements from a Set
+my_set.remove(5)    
+print("Set:", my_set)
